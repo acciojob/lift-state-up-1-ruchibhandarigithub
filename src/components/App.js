a262589eda27288b48844703fe623777b9991dff
unknown <bhandariruchi498@gmail.com>
2023-08-11 17:02:15 +0530

    daya

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,23 @@
 
-import React from "react";
+import React ,{ useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [showModal,setShowModel] = useState(false);
+  const onClick = ()=>{
+    setShowModel(true);
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent">
+      <h1>Parent Component</h1>
+      <div className="child">
+            <h2>Child Component</h2>
+            <button onClick={onClick}>Show Modal</button>
+            {showModal && <div> <h3>Modal Content</h3>
+              <p>This is modal content</p>
+            </div>}
+      </div>
+       
     </div>
   )
 }
